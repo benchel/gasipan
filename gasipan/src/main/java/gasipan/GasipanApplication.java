9a92402f5197e61642d579cd639ec133630f8272
benchel <whalswl1030@nate.com>
2022-01-03 10:40:33 +0900

    project setting

---
+++ gasipan/src/main/java/gasipan/GasipanApplication.java
@@ -0,0 +1,13 @@
+package gasipan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasipanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasipanApplication.class, args);
+	}
+
+}
---
+++ gasipan/src/test/java/gasipan/GasipanApplicationTests.java
@@ -0,0 +1,13 @@
+package gasipan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasipanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
