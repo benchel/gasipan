c1b8e7c312186380c38d5e25f4e4c4501d9a3869
benchel <whalswl1030@nate.com>
2022-02-03 15:54:28 +0900

    로그인여부에 따른 텍스트 제어

--- src/main/java/gasipan/controller/HomeController.java
+++ src/main/java/gasipan/controller/HomeController.java
@@ -1,21 +1,35 @@
 package gasipan.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import gasipan.bean.GasipanConstructer;
+import gasipan.service.UserService;
+import gasipan.vo.UsersVo;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/")
 @Slf4j
 public class HomeController {
 	
+	private final HttpSession session;
+	private final UserService userService;
+	
 	@GetMapping("/")
 	public String index(ModelMap modelMap) {
+		
+		String SESSION_USER_ID = (String) session.getAttribute(GasipanConstructer.SESSION_USER_ID);
+		UsersVo user = userService.loadUserByUsername(SESSION_USER_ID);
+		
 		try {
-			modelMap.addAttribute("appleColor", "red");
+			modelMap.addAttribute("ssUser", user);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
