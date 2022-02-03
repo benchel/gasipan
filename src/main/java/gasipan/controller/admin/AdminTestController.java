630a6b4bbad6e08ed5ae1a1e6e9dce985fa550a7
benchel <whalswl1030@nate.com>
2022-02-03 17:00:09 +0900

    관리자 정보 표시

--- src/main/java/gasipan/controller/admin/AdminTestController.java
+++ src/main/java/gasipan/controller/admin/AdminTestController.java
@@ -1,15 +1,40 @@
 package gasipan.controller.admin;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import gasipan.bean.GasipanConstructer;
+import gasipan.service.UserService;
+import gasipan.vo.UsersVo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin/*")
+@Slf4j
 public class AdminTestController {
 
+	private final HttpSession session;
+	private final UserService userService;
+	
 	@GetMapping("/")
-	public String view() {
+	public String view(ModelMap modelMap) {
+		
+		try {
+			String SESSION_USER_ID = (String) session.getAttribute(GasipanConstructer.SESSION_USER_ID);
+			UsersVo user = userService.loadUserByUsername(SESSION_USER_ID);
+			
+			modelMap.addAttribute("admin", user);
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+		
 		return "admin/index";
 	}
 	
