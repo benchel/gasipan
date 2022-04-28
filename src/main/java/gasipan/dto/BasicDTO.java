0056a62ee1ddd8b9ee0d333b5e27920c82151fc7
benchel <whalswl1030@nate.com>
2022-04-28 11:36:43 +0900

    BasicVO, BasicDTO 추가

---
+++ src/main/java/gasipan/dto/BasicDTO.java
@@ -0,0 +1,6 @@
+package gasipan.dto;
+
+public class BasicDTO {
+	private String rnum;
+	private int message;
+}
---
+++ src/main/java/gasipan/vo/BasicVO.java
@@ -0,0 +1,23 @@
+package gasipan.vo;
+
+
+public class BasicVO {
+	
+	private String rnum;
+	private int message;
+	
+	public String getRnum() {
+		return rnum;
+	}
+	public void setRnum(String rnum) {
+		this.rnum = rnum;
+	}
+	public int getMessage() {
+		return message;
+	}
+	public void setMessage(int message) {
+		this.message = message;
+	}
+	
+	
+}
