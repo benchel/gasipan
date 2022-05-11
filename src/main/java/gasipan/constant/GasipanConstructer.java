c649ece89418ffbb0476d656c35ec80dda5bd325
benchel <whalswl1030@nate.com>
2022-05-11 13:35:13 +0900

    UserInterceptor preHandle(), postHandle() 추가

--- src/main/java/gasipan/constant/GasipanConstructer.java
+++ src/main/java/gasipan/constant/GasipanConstructer.java
@@ -1,6 +1,6 @@
 package gasipan.constant;
 
-public class GasipanConstructer {
+public class GasipanConstructer {	
 	public static final String SESSION_USER_ID = "ssUserId";
 	public static final String SESSION_USER_NAME = "ssUserName";
 	public static final String SESSION_USER_EMAIL = "ssUserEmail";
