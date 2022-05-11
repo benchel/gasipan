20ff2dfe602954ff6db91b2978926aeb603c6ca4
benchel <whalswl1030@nate.com>
2022-05-11 13:36:44 +0900

    UserInterceptor preHandle(), postHandle() 추가

--- src/main/java/gasipan/interceptor/UserInterceptor.java
+++ src/main/java/gasipan/interceptor/UserInterceptor.java
@@ -2,15 +2,9 @@ package gasipan.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
-import gasipan.constant.GasipanConstructer;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -23,27 +17,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserInterceptor implements HandlerInterceptor {
 	
+	/**
+	 * Controller 실행 직전에 동작
+	 * return 값이 true인 경우 정상적으로 진행, false인 경우 컨트롤러로 진입하지 않고 실행 종료
+	 * @param handler : HandlerMapping이 찾은 Controller를 의미함
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		log.info("preHandle() 실행");
+		return true;
+	}
+	
+	/**
+	 * 뷰(view)가 렌더링되기 전에 수행된다.
+	 * 뷰에 출력될 데이터 조작 가능
+	 */
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// 인증객체 가져오기
-		HttpSession session = request.getSession();
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		
-		// 인증객체가 null 값이 아니면 
-		if(authentication != null) {
-			// 인증객체의 타입 확인
-			if(authentication.getPrincipal() instanceof User) {
-				User user = User.class.cast(authentication.getPrincipal());
-				
-				// 스프링 시큐리티에는 로그인 정보가 있는데 HTTP 세션에는 로그인 관련 정보가 없는 경우 로그아웃 처리
-				if(session.getAttribute(GasipanConstructer.SESSION_USER_ID) == null) {
-					log.info("session timeout : {}", user.getUsername());
-					response.sendRedirect("/user/logout");
-				}
-				
-			}
-		} // if(authentication != null) {}
+		log.info("postHandle() 실행");
 	}
 	
 }
